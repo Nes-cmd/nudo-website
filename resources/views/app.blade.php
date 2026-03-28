f03e2f15d53636c0a22741718f53cf7d577a6386
Nesru <nesrusadik6@gmail.com>
2026-03-28 16:41:53 +0300

    The vite things

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @vite(['resources/js/app.jsx', 'resources/css/app.css'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
